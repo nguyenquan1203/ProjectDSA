f1de4a1f3cb031e6eac0af888844021e14cb383e
NguyenQuan <96571848+nguyenquan1203@users.noreply.github.com>
2026-03-14 15:16:40 +0700

    Revert "Add Bitstream, file and commontypes header"

--- File.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILEUTIL_H
-#define FILEUTIL_H
-
-#include <string>
-
-std::string readFile(const std::string &filename);
-void saveToFile(const std::string &filename, const std::string &data);
-
-#endif
