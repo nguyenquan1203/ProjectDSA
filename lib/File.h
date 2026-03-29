e56e12e4dacc5a63d0c21eebdbd38f14bf9a50af
NguyenQuan <96571848+nguyenquan1203@users.noreply.github.com>
2026-03-29 17:17:46 +0700

    Add header functions for file operations

--- lib/File.h
+++ lib/File.h
@@ -2,8 +2,10 @@
 #define FILEUTIL_H
 
 #include <string>
-
+#include "Bitstream.h"
 std::string readFile(const std::string &filename);
 void saveToFile(const std::string &filename, const std::string &data);
-
+void writeHeaderExtension(OutBitStream &out, const std::string &ext);
+std::string readHeaderExtension(InBitStream &in);
+std::string getBaseName(const std::string &filename);
 #endif
