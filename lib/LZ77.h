922f2fea5a9a0f4fc3e02e0f09414f83a7ce8748
NguyenQuan <96571848+nguyenquan1203@users.noreply.github.com>
2026-03-29 17:25:33 +0700

    Modify LZ77 compression parameters and add readFile
    Updated the parameters for lz77_compress function and added readFile function declaration.

--- lib/LZ77.h
+++ lib/LZ77.h
@@ -2,18 +2,9 @@
 #include <string>
 #include <cstdint>
 #include "CommonTypes.h"
-// struct Token
-// {
-//     bool isMatch;
-//     char literal;
-//     uint16_t distance;
-//     uint16_t length;
-// };
-std::vector<Token> lz77_compress(
-    const std::string &s,
-    uint16_t windowSize = 32768,
-    uint16_t maxMatchLen = 256);
+
+std::vector<Token> lz77_compress(const std::string &s, int windowSize = 1024, int maxMatchLen = 1024);
+
 std::string lz77_decode(const std::vector<Token> &tokens);
-// int max(int a, int b);
-// int min(int a, int b);
-// std::string substr(const std::string &s, int pos, int len);
+
+std::string readFile(const std::string &filename);
